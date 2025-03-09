d6da69b784d36d9c29db96b9d0a3d3ad49c506aa
kTrzcinskii <kt.trzcinski@gmail.com>
2025-03-09 22:24:55 +0100

    Implement result pattern

--- TickAPI/TickAPI/Common/Result/Result.cs
+++ TickAPI/TickAPI/Common/Result/Result.cs
@@ -1,6 +1,28 @@
 ﻿namespace TickAPI.Common.Result;
 
-public class Result
+public record Result<T>
 {
+    public bool IsSuccess { get; }
+    public bool IsError => !IsSuccess;
+    public T? Value { get; }
+    public int StatusCode { get; }
+    public string ErrorMsg { get; }
     
+    private Result(bool isSuccess, T? value = default, int statusCode = StatusCodes.Status200OK, string errorMsg = "")
+    {
+        IsSuccess = isSuccess;
+        Value = value;
+        StatusCode = statusCode;
+        ErrorMsg = errorMsg;
+    }
+
+    public static Result<T> Success(T value)
+    {
+        return new Result<T>(true, value);
+    }
+
+    public static Result<T> Failure(int statusCode, string errorMsg)
+    {
+        return new Result<T>(false, default, statusCode, errorMsg);
+    }
 }
