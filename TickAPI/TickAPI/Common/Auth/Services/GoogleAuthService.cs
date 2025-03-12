b377e1287570dc43d2d6e03aa28d3ad0d411a072
kubapoke <q.paczka@gmail.com>
2025-03-12 20:43:32 +0100

    implemented initial version of GoogleAuthService.cs

--- TickAPI/TickAPI/Common/Auth/Services/GoogleAuthService.cs
+++ TickAPI/TickAPI/Common/Auth/Services/GoogleAuthService.cs
@@ -1,8 +1,33 @@
-﻿using TickAPI.Common.Auth.Abstractions;
+﻿using Google.Apis.Auth;
+using TickAPI.Common.Auth.Abstractions;
+using TickAPI.Common.Result;
 
 namespace TickAPI.Common.Auth.Services;
 
 public class GoogleAuthService : IAuthService
 {
-    
+    private readonly IConfiguration _configuration;
+
+    public GoogleAuthService(IConfiguration configuration)
+    {
+        _configuration = configuration;
+    }
+
+    public async Task<Result<string>> LoginAsync(string idToken)
+    {
+        try
+        {
+            var payload = await GoogleJsonWebSignature.ValidateAsync(idToken,
+                new GoogleJsonWebSignature.ValidationSettings
+                {
+                    Audience = [_configuration["Authentication:Google:ClientId"]]
+                });
+            
+            return Result<string>.Success(payload.Email);
+        }
+        catch (Exception ex)
+        {
+            return Result<string>.Failure(StatusCodes.Status401Unauthorized, "Invalid Google token");
+        }
+    }
 }
--- TickAPI/TickAPI/Program.cs
+++ TickAPI/TickAPI/Program.cs
@@ -57,11 +57,10 @@ builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
         options.TokenValidationParameters = new TokenValidationParameters
         {
             ValidateIssuer = true,
-            ValidateAudience = true,
+            ValidateAudience = false,
             ValidateLifetime = true,
             ValidateIssuerSigningKey = true,
             ValidIssuer = builder.Configuration["Authentication:Jwt:Issuer"],
-            ValidAudience = builder.Configuration["Authentication:Jwt:Issuer"],
             IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Authentication:Jwt:SecurityKey"]))
         };
     });
