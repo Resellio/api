b679fe50e6fbd7d249a5b798ed0b8514467d183f
kTrzcinskii <kt.trzcinski@gmail.com>
2025-03-16 23:41:40 +0100

    Add our own `Authorize` attribute built on top of the original one to ensure enum type-safety

---
+++ TickAPI/TickAPI/Common/Auth/Attributes/AuthorizeWithPolicy.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using TickAPI.Common.Auth.Enums;
+
+namespace TickAPI.Common.Auth.Attributes;
+
+public class AuthorizeWithPolicy : AuthorizeAttribute
+{
+    public AuthorizeWithPolicy(AuthPolicies policy)
+    {
+        Policy = policy.ToString();
+    }
+}
