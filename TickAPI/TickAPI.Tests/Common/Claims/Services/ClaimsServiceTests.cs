b5658df6156bc6b4a083337e19793225c90aa7e6
kTrzcinskii <kt.trzcinski@gmail.com>
2025-03-31 09:38:08 +0200

    Add tests for `ClaimsService`

---
+++ TickAPI/TickAPI.Tests/Common/Claims/Services/ClaimsServiceTests.cs
@@ -0,0 +1,49 @@
+﻿using System.Security.Claims;
+using Microsoft.AspNetCore.Http;
+using TickAPI.Common.Claims.Abstractions;
+using TickAPI.Common.Claims.Services;
+
+namespace TickAPI.Tests.Common.Claims.Services;
+
+public class ClaimsServiceTests
+{
+    private readonly IClaimsService _claimsService;
+
+    public ClaimsServiceTests()
+    {
+        _claimsService = new ClaimsService();
+    }
+
+    [Fact]
+    public void GetEmailFromClaims_WhenEmailInClaims_ShouldReturnEmail()
+    {
+        // Arrange
+        var email = "test@gmail.com";
+        var claims = new List<Claim>
+        {
+            new Claim(ClaimTypes.Email, email)
+        };
+        
+        // Act
+        var result = _claimsService.GetEmailFromClaims(claims);
+        
+        // Assert
+        Assert.True(result.IsSuccess);
+        Assert.Equal(email, result.Value!);
+    }
+    
+    [Fact]
+    public void GetEmailFromClaims_WhenEmailNotInClaims_ShouldReturnFailure()
+    {
+        // Arrange
+        var claims = new List<Claim>();
+        
+        // Act
+        var result = _claimsService.GetEmailFromClaims(claims);
+        
+        // Assert
+        Assert.True(result.IsError);
+        Assert.Equal(StatusCodes.Status400BadRequest, result.StatusCode);
+        Assert.Equal("missing email claim", result.ErrorMsg);
+    }
+}
