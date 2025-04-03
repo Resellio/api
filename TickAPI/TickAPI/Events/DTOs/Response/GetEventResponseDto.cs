a4c7df0518a150a0938f4bfb901efdc2f2a90f4b
kTrzcinskii <kt.trzcinski@gmail.com>
2025-04-03 17:32:48 +0200

    Create DTOs for getting events as organizer

---
+++ TickAPI/TickAPI/Events/DTOs/Response/GetEventResponseDto.cs
@@ -0,0 +1,14 @@
+﻿using TickAPI.Events.Models;
+
+namespace TickAPI.Events.DTOs.Response;
+
+public record GetEventResponseDto(
+    string Name,
+    string Description,
+    DateTime StartDate,
+    DateTime EndDate,
+    uint? Minimumage,
+    List<GetEventResponseCategoryDto> Categories,
+    EventStatus Status,
+    GetEventResponseAddressDto Addres
+);
