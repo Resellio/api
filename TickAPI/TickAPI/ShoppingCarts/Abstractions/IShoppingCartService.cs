64e56701fed724ab2b5746d61682d1c702bf2c2d
kubapoke <q.paczka@gmail.com>
2025-05-03 00:57:59 +0200

    added the base of ShoppingCartService.cs methods (headers still require changing)

--- TickAPI/TickAPI/ShoppingCarts/Abstractions/IShoppingCartService.cs
+++ TickAPI/TickAPI/ShoppingCarts/Abstractions/IShoppingCartService.cs
@@ -1,6 +1,11 @@
-﻿namespace TickAPI.ShoppingCarts.Abstractions;
+﻿using TickAPI.Common.Results;
+
+namespace TickAPI.ShoppingCarts.Abstractions;
 
 public interface IShoppingCartService
 {
-    
+    public Task<Result> AddTicketAsync();
+    public Task<Result> GetTicketsAsync();
+    public Task<Result> RemoveTicketAsync();
+    public Task<Result> CheckoutAsync();
 }
--- TickAPI/TickAPI/ShoppingCarts/Services/ShoppingCartService.cs
+++ TickAPI/TickAPI/ShoppingCarts/Services/ShoppingCartService.cs
@@ -1,8 +1,27 @@
-﻿using TickAPI.ShoppingCarts.Abstractions;
+﻿using TickAPI.Common.Results;
+using TickAPI.ShoppingCarts.Abstractions;
 
 namespace TickAPI.ShoppingCarts.Services;
 
 public class ShoppingCartService : IShoppingCartService
 {
-    
+    public Task<Result> AddTicketAsync()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<Result> GetTicketsAsync()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<Result> RemoveTicketAsync()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<Result> CheckoutAsync()
+    {
+        throw new NotImplementedException();
+    }
 }
