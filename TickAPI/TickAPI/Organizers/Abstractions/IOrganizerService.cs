cf2fadab59e618e406208eba3c418b66419c7230
kTrzcinskii <kt.trzcinski@gmail.com>
2025-05-13 18:53:25 +0200

    Add endpoint for getting unverified organizers

--- TickAPI/TickAPI/Organizers/Abstractions/IOrganizerService.cs
+++ TickAPI/TickAPI/Organizers/Abstractions/IOrganizerService.cs
@@ -1,5 +1,7 @@
-﻿using TickAPI.Common.Results;
+﻿using TickAPI.Common.Pagination.Responses;
+using TickAPI.Common.Results;
 using TickAPI.Common.Results.Generic;
+using TickAPI.Organizers.DTOs.Response;
 using TickAPI.Organizers.Models;
 
 namespace TickAPI.Organizers.Abstractions;
@@ -11,4 +13,6 @@ public interface IOrganizerService
     public Task<Result<Organizer>> CreateNewOrganizerAsync(string email, string firstName, string lastName, string displayName);
 
     public Task<Result> VerifyOrganizerByEmailAsync(string organizerEmail);
+
+    public Task<Result<PaginatedData<GetUnverifiedOrganizerResponseDto>>> GetUnverifiedOrganizersAsync(int page, int pageSize);
 }
