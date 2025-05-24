8f447bd8d079eaa8acbde4cf5bdb3af86d9923c1
Stanisław <62651497+staszkiet@users.noreply.github.com>
2025-05-24 13:18:37 +0200

    tests

--- TickAPI/TickAPI/Tickets/Models/Ticket.cs
+++ TickAPI/TickAPI/Tickets/Models/Ticket.cs
@@ -7,7 +7,7 @@ public class Ticket
 {
     public Guid Id  { get; set; }
     public TicketType Type { get; set; }
-    public Customer? Owner { get; set; }
+    public Customer Owner { get; set; }
     public string NameOnTicket { get; set; }
     public string? Seats { get; set; }
     public bool ForResell { get; set; }
