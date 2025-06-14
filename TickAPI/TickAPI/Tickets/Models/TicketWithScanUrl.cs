b99c42b88c3fc23ab4ca43355947b8f22dc0f5c4
kTrzcinskii <kt.trzcinski@gmail.com>
2025-06-15 00:08:32 +0200

    Add option to send mail about multiple new tickets

---
+++ TickAPI/TickAPI/Tickets/Models/TicketWithScanUrl.cs
@@ -0,0 +1,13 @@
+﻿namespace TickAPI.Tickets.Models;
+
+public class TicketWithScanUrl
+{
+    public TicketWithScanUrl(Ticket ticket, string scanUrl)
+    {
+        Ticket = ticket;
+        ScanUrl = scanUrl;
+    }
+
+    public Ticket Ticket { get; set; }
+    public string ScanUrl { get; set; }
+}
